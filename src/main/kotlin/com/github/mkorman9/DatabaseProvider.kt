137edc949bc677a77728e76e1d3360968a928c12
Michal Korman <michal.korman@icloud.com>
2023-12-29 12:30:18 +0100

    add PostgreSqlDialect

--- src/main/kotlin/com/github/mkorman9/DatabaseProvider.kt
+++ src/main/kotlin/com/github/mkorman9/DatabaseProvider.kt
@@ -4,13 +4,15 @@ import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.inject.Produces
 import jakarta.inject.Singleton
 import org.ktorm.database.Database
+import org.ktorm.support.postgresql.PostgreSqlDialect
 import javax.sql.DataSource
 
 @ApplicationScoped
 class DatabaseProvider {
     @Singleton
     @Produces
-    fun database(dataSource: DataSource): Database {
-        return Database.connect(dataSource)
-    }
+    fun database(dataSource: DataSource): Database = Database.connect(
+        dataSource,
+        dialect = PostgreSqlDialect()
+    )
 }
